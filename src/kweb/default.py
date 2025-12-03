d0db20c45dbae03d025d5e79986ed463def050aa
Hany-Kim <hanykim1210@gmail.com>
2025-12-03 23:44:03 +0900

    uv 패키지 관리

--- src/kweb/default.py
+++ src/kweb/default.py
@@ -1,3 +1,11 @@
 from kweb.browser import get_app
+import uvicorn
 
 app = get_app()
+
+def main():
+    uvicorn.run(app="default:app", host="0.0.0.0", port=8765, reload=True)
+
+if __name__ == "__main__":
+    main()
+
